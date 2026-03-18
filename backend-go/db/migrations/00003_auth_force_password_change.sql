7a5a4aece301db5ddf28acb1dfb6ea81be7381e5
Yuriy Samohvalov <yuriy@samoh.ru>
2026-03-18 19:15:09 +0300

    Удалены все компоненты на Python, оставлен только Go бэкенд

--- backend-go/db/migrations/00003_auth_force_password_change.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
-ALTER TABLE vm_auth_user
-    ADD COLUMN IF NOT EXISTS must_change_password BOOLEAN NOT NULL DEFAULT FALSE;
-
--- +goose Down
-ALTER TABLE vm_auth_user
-    DROP COLUMN IF EXISTS must_change_password;
